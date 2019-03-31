15d3c3630488d03b49bc224263ebcc9192266cd7
Daniel Bovington <d.bovington@gmail.com>
2019-03-31 17:17:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   larray = []
   languages.collect do |language|
     language.upcase
-    larray << language.split("").first
+    larray << language.split().first
   end
   larray
 end
